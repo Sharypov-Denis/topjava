7ad5d344c43cdc9747f1cce5bb3081344d603266
Sharypov-Denis <sharypov.denis@gmail.com>
2020-09-21 19:34:58 +0300

    4_5_improve_code

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -25,6 +25,9 @@ public class MealsUtil {
     );
 
 
+    private MealsUtil() {
+    }
+
     public static List<MealTo> getTos(Collection<Meal> meals, int caloriesPerDay) {
         return filterByPredicate(meals, caloriesPerDay, meal -> true);
     }
